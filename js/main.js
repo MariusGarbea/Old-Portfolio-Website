89c4501fe4f704f8c30a391a6ac71643b8869ccb
Marius Garbea <mariusgarbea@ymail.com>
2016-07-18 00:45:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(".port").mouseover(function() {
     $(this).css("opacity", "0.3");
-    $(this).parent().siblings('.fa.fa-diamond').css("opacity", "1");
+    $(this).parent().siblings('.description').css("opacity", "1");
   });
   $(".port").mouseleave(function() {
     $(this).css("opacity", "1");
-    $(this).parent().siblings('.fa.fa-diamond').css("opacity", "0");
+    $(this).parent().siblings('.description').css("opacity", "0");
   });
 });
