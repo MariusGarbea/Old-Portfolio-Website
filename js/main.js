153b79e4ef46b47fc435a3a2d65841dcd86dd22f
Marius Garbea <mariusgarbea@ymail.com>
2016-07-22 06:31:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+/*http://zurb.com/playground/twentytwenty*/
+
 $(document).ready(function() {
+  var msg = "Solving problems for fun"
+  var index = 1;
+  setTimeout(function() {
+    var int = setInterval(function(){
+      $("#writetext").text(msg.substring(0, index) + "|");
+      index++;
+      if(index == msg.length) {
+        $("#writetext").text(msg.substring(0, index));
+        clearInterval(int);
+      }
+    }, 100);
+  }, 1000);
   $(".port").mouseover(function() {
     $(this).css("opacity", "0.3");
     $(this).parent().siblings('.description').css("opacity", "1");
